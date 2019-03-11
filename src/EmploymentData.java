e7e4e6d8c622bfedb056434f80c03b7a8f663b90
Shahar <shahar.broner@gmail.com>
2019-03-10 23:01:23 -0700

    Added EmploymentData class to store the information in a easier matter

---
+++ src/EmploymentData.java
@@ -0,0 +1,78 @@
+public class EmploymentData {
+    private int totalLaborForce;
+    private int employedLaborForce;
+    private int unemployedLaborForce;
+    private int unemployedPercent;
+
+    public int getTotalLaborForce() {
+        return totalLaborForce;
+    }
+
+    public void setTotalLaborForce(int totalLaborForce) {
+        this.totalLaborForce = totalLaborForce;
+    }
+
+    public int getEmployedLaborForce() {
+        return employedLaborForce;
+    }
+
+    public void setEmployedLaborForce(int employedLaborForce) {
+        this.employedLaborForce = employedLaborForce;
+    }
+
+    public int getUnemployedLaborForce() {
+        return unemployedLaborForce;
+    }
+
+    public void setUnemployedLaborForce(int unemployedLaborForce) {
+        this.unemployedLaborForce = unemployedLaborForce;
+    }
+
+    public int getUnemployedPercent() {
+        return unemployedPercent;
+    }
+
+    public void setUnemployedPercent(int unemployedPercent) {
+        this.unemployedPercent = unemployedPercent;
+    }
+
+    public String getCountyName() {
+        return CountyName;
+    }
+
+    public void setCountyName(String countyName) {
+        CountyName = countyName;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public int getFips() {
+        return fips;
+    }
+
+    public void setFips(int fips) {
+        this.fips = fips;
+    }
+
+    public EmploymentData(int totalLaborForce, int employedLaborForce, int unemployedLaborForce, int unemployedPercent, String countyName, String stateName, int fips) {
+
+        this.totalLaborForce = totalLaborForce;
+        this.employedLaborForce = employedLaborForce;
+        this.unemployedLaborForce = unemployedLaborForce;
+        this.unemployedPercent = unemployedPercent;
+        CountyName = countyName;
+        this.stateName = stateName;
+        this.fips = fips;
+    }
+
+    private String CountyName;
+    private String stateName;
+    private int fips;
+
+}
