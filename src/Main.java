71d9790302464f1b541b91f61e6f41326bf3fb06
Shahar <shahar.broner@gmail.com>
2019-03-10 22:43:10 -0700

    Added test for parse2016EducationalData method

--- src/Main.java
+++ src/Main.java
@@ -6,10 +6,12 @@ public class Main {
     //Test of utils
     public static void main(String[] args) {
         String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
+        String data1 = Utils.readFileAsString("data/Education.csv");
         //System.out.println(data);
         ArrayList<ElectionResults> results = Utils.parse2016ElectionResults(data);
+        ArrayList<EducationData> results1 = Utils.parse2016EducationData(data1);
         for (int i = 0; i < results.size(); i++) {
-            System.out.println(results.get(i).toString());
+            System.out.println(results1.get(i).toString());
         }
     }
 }
