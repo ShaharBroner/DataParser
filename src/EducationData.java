9dbbbba5bc6882d33de36f1ef9cbd78336a8994f
Shahar <shahar.broner@gmail.com>
2019-03-10 22:50:17 -0700

    Added EducationData class to store the information in a easier matter

---
+++ src/EducationData.java
@@ -0,0 +1,76 @@
+public class EducationData {
+    private double noHighSchool;
+    private double onlyHighSchool;
+    private double someCollege;
+    private double bachelorsOrMore;
+    private String CountyName;
+    private String stateName;
+    private int fips;
+
+    public double getNoHighSchool() {
+        return noHighSchool;
+    }
+
+    public void setNoHighSchool(double noHighSchool) {
+        this.noHighSchool = noHighSchool;
+    }
+
+    public double getOnlyHighSchool() {
+        return onlyHighSchool;
+    }
+
+    public void setOnlyHighSchool(double onlyHighSchool) {
+        this.onlyHighSchool = onlyHighSchool;
+    }
+
+    public double getSomeCollege() {
+        return someCollege;
+    }
+
+    public void setSomeCollege(double someCollege) {
+        this.someCollege = someCollege;
+    }
+
+    public double getBachelorsOrMore() {
+        return bachelorsOrMore;
+    }
+
+    public void setBachelorsOrMore(double bachelorsOrMore) {
+        this.bachelorsOrMore = bachelorsOrMore;
+    }
+
+    public String getCountyName() {
+        return CountyName;
+    }
+
+    public void setCountyName(String countyName) {
+        CountyName = countyName;
+    }
+
+    public String getStateName() {
+        return stateName;
+    }
+
+    public void setStateName(String stateName) {
+        this.stateName = stateName;
+    }
+
+    public int getFips() {
+        return fips;
+    }
+
+    public void setFips(int fips) {
+        this.fips = fips;
+    }
+
+    public EducationData(double noHighSchool, double onlyHighSchool, double someCollege, double bachelorsOrMore, String countyName, String stateName, int fips) {
+
+        this.noHighSchool = noHighSchool;
+        this.onlyHighSchool = onlyHighSchool;
+        this.someCollege = someCollege;
+        this.bachelorsOrMore = bachelorsOrMore;
+        CountyName = countyName;
+        this.stateName = stateName;
+        this.fips = fips;
+    }
+}
