8efe416876d1d064c63b33c63934ef592e50ca59
Shahar <shahar.broner@gmail.com>
2019-03-10 22:05:37 -0700

    Move variable to correct location

--- src/County.java
+++ src/County.java
@@ -3,6 +3,7 @@ public class County {
     private int fips;
     private Election2016 vote2016;
     private Education2016 educ2016;
+    private Employment2016 employ2016;
 
     public String getName() {
         return name;
@@ -53,5 +54,4 @@ public class County {
         this.employ2016 = employ2016;
     }
 
-    private Employment2016 employ2016;
 }
