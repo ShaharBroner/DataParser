0a6bf89fce996901c45c0808b20d9ddebd12f6c4
Shahar <shahar.broner@gmail.com>
2019-03-11 00:18:21 -0700

    Test parse2016EmploymentData method

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
     public static void main(String[] args) {
         String data = Utils.readFileAsString("data/2016_Presidential_Results.csv");
         String data1 = Utils.readFileAsString("data/Education.csv");
+        String data2 = Utils.readFileAsString("data/Unemployment.csv");
         //System.out.println(data);
         ArrayList<ElectionResults> results = Utils.parse2016ElectionResults(data);
         ArrayList<EducationData> results1 = Utils.parse2016EducationData(data1);
+        ArrayList<EmploymentData> results2 = Utils.parse2016EmploymentData(data2);
         for (int i = 0; i < results.size(); i++) {
-            System.out.println(results1.get(i).toString());
+            System.out.println(results2.get(i).getUnemployedPercent());
         }
     }
 }
